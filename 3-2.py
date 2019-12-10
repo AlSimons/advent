cea2e439504d74e4a7a75c9501b6ecea2069cb30
Al Simons <al@simonshome.org>
2019-12-10 17:40:14 +0000

    3-2.  This was not as easy as it appeared at first

--- 3-2.py
+++ 3-2.py
@@ -101,6 +101,13 @@ class PathSegment(object):
         else:
             new_x = self.prev_location.x
             new_y = self.prev_location.y + distance
+
+        # Track the path distance.
+        if prev_location is None:
+            self.path_distance = abs(distance)
+        else:
+            self.path_distance = prev_location.path_distance + \
+                abs(distance)
         self.location = Point(new_x, new_y)
 
     def is_vertical(self):
@@ -195,49 +202,129 @@ class PathSegment(object):
             # My y coordinate is fixed.
             return True, Point(other_fixed, my_fixed)
 
+    def retract_to(self, location):
+        """
+        In traversing paths, we may have traveled past an intersection.
+        This method allows us to compute how far past an intersection a
+        path segment went, so we can pull it back out.
+        :param location: The location of the intersection we want to retreat
+            to.
+        :return: The distance this path segment went past the intersection.
+        """
+        if self.is_vertical():
+            # Sanity check: is our X value the X value of the intersection?
+            if self.location.x != location.x:
+                raise ValueError("Segment X value doesn't match intersection: "
+                                 "Segment is {}, location is {}.".format(
+                                    self.location.x, location.x
+                                    ))
+            # And check that the intersection's Y value is between our current
+            # and previous Y values.
+            curr = self.location.y
+            prev = self.prev_location.y
+            # For simplicity, make curr > prev, and remember that we did so
+            flipped = False
+            if curr < prev:
+                curr, prev = prev, curr
+                flipped = True
+            if curr < location.y or location.y < prev:
+                raise ValueError("Segment Y values don't match intersection: "
+                                 "Segment is curr {}, prev {}, location "
+                                 "is {}.".format(
+                                    self.location.y,
+                                    self.prev_location.y,
+                                    location.y
+                                    ))
+            # OK, now do the calculation.
+            if flipped:
+                # We were traveling down.
+                overshot = location.y - prev
+            else:
+                overshot = curr - location.y
+            return overshot
+        else:
+            # OK, now, unfortunately, have to do the same thing all over again
+            # for the case that we're moving horizontally.
+            if self.location.y != location.y:
+                raise ValueError("Segment Y value doesn't match intersection: "
+                                 "Segment is {}, location is {}.".format(
+                                    self.location.y, location.y
+                                    ))
+            # And check that the intersection's X value is between our current
+            # and previous X values.
+            curr = self.location.x
+            prev = self.prev_location.x
+            # For simplicity, make curr > prev, and remember we did so.
+            flipped = False
+            if curr < prev:
+                curr, prev = prev, curr
+                flipped = True
+            if curr < location.x or location.x < prev:
+                raise ValueError("Segment X values don't match intersection: "
+                                 "Segment is curr {}, prev {}, location "
+                                 "is {}.".format(
+                                    self.location.x,
+                                    self.prev_location.x,
+                                    location.x
+                                    ))
+            if flipped:
+                # We were traveling right to left.
+                overshot = location.x - prev
+            else:
+                overshot = curr - location.x
+            return overshot
+
     def __repr__(self):
-        return "Loc {}, Prev loc {}".format(self.location, self.prev_location)
+        return "[Loc {}, Prev loc {}, Dist {}]".format(
+            self.location, self.prev_location,
+            self.path_distance)
 
 
 def main():
-    """
-
-    :return:
-    """
     first_path = []
     second_path = []
     with open('3-1 sample input.txt') as f:
         for path in (first_path, second_path):
+            prev_segment = None
             path_line = f.readline()
-            for segment in path_line.split(','):
-                if not path:
-                    # Our first time; there is no previous path segment
-                    prev = None
-                else:
-                    prev = path[-1]
-                path.append(PathSegment(prev, segment))
+            for path_command in path_line.split(','):
+                new_segment = PathSegment(prev_segment, path_command)
+                path.append(new_segment)
+                prev_segment = new_segment
 
     # Now we have two complete paths.  Time to check for intersections.
     # Currently O(N^2). Could figure out some sorting mechanism and maybe
     # get more efficient.  But not now.
     shortest = 999999999999999
-
-    for segment_1 in first_path:
-        for segment_2 in second_path:
+    for idx_1, segment_1 in enumerate(first_path):
+        for idx_2, segment_2 in enumerate(second_path):
             does_intersect, location = segment_1.intersects(segment_2)
             if does_intersect:
-                print("Seg_1", segment_1)
-                print("Seg_2", segment_2)
-                print("Location", location)
-                possible = location.x + location.y
-                # A distance of 0 happens at the very beginning, at the origin.
-                if possible < shortest and possible > 0:
+                # Don't count intersection at the starting point origin.
+                # But do if later on they happen to intersect back at the
+                # origin.
+                if idx_1 + idx_2 == 0:
+                    continue
+                possible = segment_1.path_distance + segment_2.path_distance
+
+                # The intersection may have (probably did) happen before
+                # the end of the last two path segments.  We have to backtrack
+                # to the last intersection.
+                possible -= segment_1.retract_to(location)
+                possible -= segment_2.retract_to(location)
+
+                if possible < shortest:
                     shortest = possible
+                # Since we're going for the shortest path, and walking the
+                # points in path order, once we have one intersection for
+                # first_path[x], second_path, there will be no shorter paths
+                # with this first_path segment. Break the second_path loop
+                # to pick up the next first_path segment.
+                break
 
-    # If I've done everything right, we can now print out the shortest
-    # Manhattan distance, and move on to the next problem!
-    print("The shortest Manhattan distance is {}".format(shortest))
-
+    # If I've done everything right, we can now print out the path length,
+    # and move on to the next problem!
+    print("The shortest path distance is {}".format(shortest))
     # That's all, folks!
 
 
